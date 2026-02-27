2c440647006e72240813b18dc81035a33439d6a4
Gerald <geraldsang1550@gmail.com>
2026-02-27 20:52:10 +0300

    WRTE

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -37,7 +37,6 @@ export default function Partners() {
         notes: data.notes,
         type: data.type,
         status: "pending",
-          read_only: null,
       });
 
       if (error) throw error;
